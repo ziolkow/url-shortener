ee66b94cd6608d4b19add66fdd41eb1c88db2b8f
Slawomir Ziolkowski <slawomir.ziolkowski@sixt.com>
2022-07-09 12:58:11 +0200

    attempt to add info and help endpoints

--- src/main/kotlin/org/ziolkow/controller/RedirectController.kt
+++ src/main/kotlin/org/ziolkow/controller/RedirectController.kt
@@ -15,7 +15,7 @@ interface RedirectController {
     fun redirect(@PathVariable("urlShort") urlShort: String) : HttpResponse<String>
 }
 
-@Controller("/")
+@Controller("/red")
 class RedirectControllerControllerImpl (
     private val urlManagementService : UrlManagementService
     ) : RedirectController {
